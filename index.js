0f3e543fc8b0e9ce14e57adaca3b7622bdff74d7
rayhan60611 <pecon.alok@gmail.com>
2023-08-05 16:54:54 +0600

    delete a single user from  mongodb atlas

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -49,6 +49,17 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
       console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    });
+
+    //delete user
+    app.delete("/users/:Id", async (req, res) => {
+      const id = req.params.Id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      if (result.deletedCount === 1) {
+        console.log("Successfully deleted one document.");
+        res.send(result);
+      }
     });
 
     // Send a ping to confirm a successful connection
